def8e5ee2480b802f4425b07938e79abed4f6b43
jarod-johnson-23 <93343444+jarod-johnson-23@users.noreply.github.com>
2021-11-02 19:20:10 -0700

    Add files via upload

---
+++ Nurse.java
@@ -0,0 +1,13 @@
+package application;
+
+public class Nurse {
+	private String fName;
+	private String lName;
+	private String email;
+	
+	public Nurse(String fName, String lName, String email) {
+		this.fName = fName;
+		this.lName = lName;
+		this.email = email;
+	}
+}

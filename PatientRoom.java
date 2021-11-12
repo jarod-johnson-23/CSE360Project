c578372784cb0b4546b2ed45b9c05408f8f1c95b
nyx147 <93344649+nyx147@users.noreply.github.com>
2021-11-11 18:01:57 -0700

    Add files via upload

---
+++ PatientRoom.java
@@ -0,0 +1,16 @@
+package application;
+
+public class PatientRoom {
+
+	private Doctor doctor_in_room;
+	private Nurse nurse_in_room;
+	private Patient current_patient;
+
+	public PatientRoom(Doctor d, Nurse n, Patient p){
+		doctor_in_room = d;
+		nurse_in_room = n;
+		current_patient = p;
+
+	}
+
+}

3fcaabec4ebd0f99bd682e0d1431c75572e17500
jarod-johnson-23 <93343444+jarod-johnson-23@users.noreply.github.com>
2021-11-17 00:21:08 -0700

    Update PatientMessages.java

--- PatientMessages.java
+++ PatientMessages.java
@@ -32,7 +32,10 @@ import javafx.scene.layout.GridPane;
 public class PatientMessages extends BorderPane {
 	private int width = 1500;
 	private int height = 1000;
-	private Button back, info, messages, pastVisits, scheduleVisit, backToHome;
+	private Button back, info, messages, pastVisits, scheduleVisit, backToHome, inbox, write, sent;
+	private Label mTitle, from, subject, body, blank;
+	private Font titleFont;
+	
 
 	public PatientMessages() throws FileNotFoundException {
 		Patient patient = WelcomePage.getLoggedInPatient();
@@ -41,16 +44,42 @@ public class PatientMessages extends BorderPane {
 		BackgroundImage defaultImage = new BackgroundImage(defaultBgPic, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT, BackgroundSize.DEFAULT);
 		Background defaultBg = new Background(defaultImage);
 		
+		// set patient background
+		BackgroundFill patHpBgFill = new BackgroundFill(Color.WHITESMOKE, new CornerRadii(10), new Insets(100, 200, 150, 10));
+		Background whiteBg = new Background(patHpBgFill);
+		
+		titleFont = new Font("Cambria", 24);
+		
 		// define vBox
 		VBox tabs = new VBox();
 		tabs.setPadding(new Insets(20, 0 ,20, 20));
 		tabs.setPrefSize(200, 65);
+		
+		VBox moreTabs = new VBox();
+		moreTabs.setPadding(new Insets(50, 0, 20, -10));
+		moreTabs.setPrefSize(200,  65);
+		
+		//define grid for center of screen
+		GridPane grid = new GridPane();
+		grid.setPadding(new Insets(20, 20, 20, 20));
+		grid.setBackground(whiteBg);
+		
+		mTitle = new Label("");
+		from = new Label("From: ");
+		subject = new Label("Subject: ");
+		body = new Label("");
+		blank = new Label("");
+		body.setWrapText(true);
+		body.setMaxWidth(400);
 		
 		info = new Button("Information");
 		messages = new Button("Messages");
 		pastVisits = new Button("Past Visits");
 		scheduleVisit = new Button("Schedule A Visit");
 		backToHome = new Button("Logout");
+		inbox = new Button("Inbox");
+		write = new Button("Write");
+		sent = new Button("Sent");
 		
 		// specify minimum size
 		info.setMinSize(tabs.getPrefWidth(), tabs.getPrefHeight());
@@ -58,6 +87,10 @@ public class PatientMessages extends BorderPane {
 		pastVisits.setMinSize(tabs.getPrefWidth(), tabs.getPrefHeight());
 		scheduleVisit.setMinSize(tabs.getPrefWidth(), tabs.getPrefHeight());
 		backToHome.setMinSize(tabs.getPrefWidth(), tabs.getPrefHeight());
+		inbox.setMinSize(tabs.getPrefWidth(), tabs.getPrefHeight());
+		write.setMinSize(tabs.getPrefWidth(), tabs.getPrefHeight());
+		sent.setMinSize(tabs.getPrefWidth(), tabs.getPrefHeight());
+		
 		
 		// link source nodes with handler objects
 		backToHome.setOnMouseClicked(new LogoutHandler());
@@ -66,19 +99,39 @@ public class PatientMessages extends BorderPane {
 		pastVisits.setOnMouseClicked(new PastVisitsHandler());
 		scheduleVisit.setOnMouseClicked(new ScheduleVisitHandler());
 		
+		mTitle.setText("Messaging Center");
+		mTitle.setFont(titleFont);
+		
 		// add elements to tabs
 		tabs.getChildren().addAll(info, messages, pastVisits, scheduleVisit, backToHome);
+		moreTabs.getChildren().addAll(inbox, write, sent);
+		
+		grid.add(mTitle, 0, 0);
+		grid.add(blank, 0, 1);
+		grid.add(moreTabs, 0, 3);
+		
 				
 		// set elements to borderPane
 		this.setLeft(tabs);	
 		this.setBackground(defaultBg);
 		this.getLeft().setStyle("-fx-background-color: darkgrey");
+		this.setCenter(grid);
 		
+		inbox.setOnAction(e -> {
+			
+		});
 		
+		write.setOnAction(e -> {
+			
+		});
+		
+		sent.setOnAction(e -> {
+			
+		});
 		
 	}
 	
-	// information
+		// information
 		private class InfoHandler implements EventHandler<MouseEvent>
 		{
 			public void handle(MouseEvent infoEvent)
